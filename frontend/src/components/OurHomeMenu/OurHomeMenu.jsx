b00f183b84016d6bf84baab65fd23f2b8387e2e0
Rajesh Malik <neha>
2025-09-03 21:03:09 +0530

    Add to Cart feature added in OurHomeMenu file

--- frontend/src/components/OurHomeMenu/OurHomeMenu.jsx
+++ frontend/src/components/OurHomeMenu/OurHomeMenu.jsx
@@ -42,7 +42,45 @@ const OurHomeMenu = () => {
                     <div key={item.id} className="relative bg-amber-900/20 rounded-2xl overflow-hidden border border-amber-800/30 backdrop-blur-sm flex flex-col transition-all duration-500"
                     style={{'--index':i}}>
                         <div className="relative h-48 sm:h-56 md:h-60 flex items-center justify-center bg-black/10">
-                        <img src={item.image} alt={item.name}/>
+                        <img src={item.image} alt={item.name}
+                        className="max-h-full max-w-full object-contain transition-all duration-700"/>
+                        </div>
+                        <div className="p-4 sm:p-6 flex flex-col flex-grow">
+                            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-16 h-1 bg-gradient-to-r from-transition via-amber-800/50 to-transparent opacity-50 transition-all duration-300"/>
+                            <h3 className="text-xl sm:text-2xl mb-2 font-dancingscript text-amber-100 transition-colors">
+                                {item.name}
+                            </h3>
+                            <p className="text-amber-100/80 text-xs sm:text-sm mb-4 font-cinzel leading-relaxed">
+                            {item.description}</p>
+
+                            <div className="mt-auto flex items-center gap-4 justify-between">
+                                <div className="bg-amber-100/10 backdrop-blur-sm px-3 py-1 rounded-2xl shadow-lg">
+                                <span className="text-xl font-bold text-amber-300 font-dancingscript">
+                                    ₹{item.price}
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                {quantity>0 ?(
+                                    <>
+                                    <button classname="w-8 h-8 rounded-full bg-amber-900/40 flex items-center justify-center hover:bg-amber-800/50 transition-colors" onClick={()=>quantity>1?addToCart(item,quantity-1): removeFromCart(item.id)}>
+                                    <FaMinus className="text-amber-100"/>
+                                    </button>
+                                    <span className="w-8 text-center text-amber-100">
+                                      {quantity}
+                                      </span>
+                                      <button classname="w-8 h-8 rounded-full bg-amber-900/40 flex items-center justify-center hover:bg-amber-800/50 transition-colors" onClick={()=>addToCart(item,quantity+1)}>
+                                      <FaPlus className="text-amber-100"/>
+                                      </button>
+                                    </>
+                                ):(
+                                  <button className="bg-amber-900/40 px-4 py-1.5 rounded-full font-cinzel text-xs uppercase sm:text-sm tracking-wider transition-transform duration hover:scale-110 hover:shadow-lg hover:shadow-amber-900/20 relative overflow-hidden border border-amber-800/50">
+                                    <span className="relative z-10 text-xs text-black">
+                                      Add to Cart
+                                    </span>
+                                  </button>
+                                )}
+                            </div>
+                        </div>
                         </div>
                         </div>
                 )
