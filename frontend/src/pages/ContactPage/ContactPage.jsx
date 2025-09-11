d2fe52726f933a9005cbdc6aeccf01a9c928d874
Muskan8878h <muskan8878h@gmail.com>
2025-09-11 10:51:50 +0530

    contact page

--- frontend/src/pages/ContactPage/ContactPage.jsx
+++ frontend/src/pages/ContactPage/ContactPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+import Contact from '../../components/Contact/Contact'
 
 
 const ContactPage = () => {
   return (
     <>
       <Navbar />
+      <Contact />
       <Footer />
     </>
   )
